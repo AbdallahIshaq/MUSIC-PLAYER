75905f1ff47255ac92cbb23dc05a2adb056b49c5
AbdallahIshak <abdallahmoze15@gmail.com>
2024-07-04 18:50:56 +0300

    Task2

---
+++ music.js
@@ -0,0 +1,15 @@
+let trackList = [
+    {
+      name: "Lobby-Time",
+      path: "D:/CodeAlpha_Project_Name/Lobby-Time(chosic.com).mp3",
+      img: "D:/CodeAlpha_Project_Name/img3.jpg",
+      singer: "Abdallah Ishaq",
+    },
+    {
+      name: "VEL",
+      path: "D:/CodeAlpha_Project_Name/VEL.mp3",
+      img: "D:/CodeAlpha_Project_Name/img2.jpg",
+      singer: "Abdallah Ishaq",
+    },
+   
+  ];
